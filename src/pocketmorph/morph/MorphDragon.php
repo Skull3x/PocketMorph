b8654fc6c4f264c19a214ac90ce13ba0b9822b1f
Joshua <joshua67.r@gmail.com>
2017-01-14 15:58:20 -0500

    Update MorphDragon.php

--- src/pocketmorph/morph/MorphDragon.php
+++ src/pocketmorph/morph/MorphDragon.php
@@ -1,1 +1,26 @@
-
+<?php
+namespace pocketmorph\morph;
+use pocketmine\network\protocol\AddEntityPacket;
+use pocketmine\Player;
+class MorphDragon extends Morph implements MorphEntity
+{
+    const NETWORK_ID = 53;
+    public function getName()
+    {
+        return "Dragon";
+    }
+    public function spawnTo(Player $player)
+    {
+        $pk = new AddEntityPacket();
+        $pk->eid = $this->getId();
+        $pk->type = self::NETWORK_ID;
+        $pk->x = $this->x;
+        $pk->y = $this->y;
+        $pk->z = $this->z;
+        $pk->yaw = $this->yaw;
+        $pk->pitch = $this->pitch;
+        $pk->metadata = $this->dataProperties;      
+      $player->dataPacket($pk);
+        parent::spawnTo($player);
+    }
+}
